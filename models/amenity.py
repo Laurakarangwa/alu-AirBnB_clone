b8dd7e0c3a03c959c9633c0ed90984fc6334f461
Laurakarangwa <l.kwizera1@alustudent.com>
2025-09-14 22:06:07 +0200

    I have created required files and dir

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
-"""Defining the Amenity class."""
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """
-    Representing an amenity.
+    """Represent an amenity.
     Attributes:
         name (str): The name of the amenity.
     """
